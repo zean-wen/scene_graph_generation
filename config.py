043c71148dc008b77be5868807600f3ba8f6d5fe
Zean Wen <zwen2780@uni.sydney.edu.au>
2021-01-08 22:09:07 +1100

    save features to.p

--- config.py
+++ config.py
@@ -8,26 +8,26 @@ class BaseConfig:
 
 
 class WordEmbeddingConfig(BaseConfig):
-    glove_dictionary_file: str
-    glove_word_matrix_file: str
-    fasttext_dictionary_file: str
-    fasttext_word_matrix_file: str
+    glove_dictionary_file: str = None
+    glove_word_matrix_file: str = None
+    fasttext_dictionary_file: str = None
+    fasttext_word_matrix_file: str = None
 
 
 class Config(BaseConfig):
-    tiers: str
-    save_dir: str
-    ids_map_dir: str
-    data_root: str
-    scene_graph_folder: str
-    ocr_folder: str
-    visual_feature_folder: str
+    tiers: str = None
+    save_dir: str = None
+    ids_map_dir: str = None
+    data_root: str = None
+    scene_graph_folder: str = None
+    ocr_folder: str = None
+    visual_feature_folder: str = None
     word_emb_config: WordEmbeddingConfig = WordEmbeddingConfig()
 
     @ classmethod
     def copy_from_args(cls, args):
         for key, value in vars(args).items():
             if hasattr(cls, key):
-                setattr(object, key, value)
+                setattr(cls, key, value)
         cls.word_emb_config = WordEmbeddingConfig.copy_from_args(args)
         return cls()
